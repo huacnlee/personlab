46b338511af45ddb4c236cba5dded0b1e7581a3a
huacnlee <jason@jason-laptop.(none)>
2009-11-10 00:00:20 +0800

    1.upload install.sh2.removed newrelic plugin,we don't need it now.

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 # sudo apt-get install ruby rubygems irb rake ruby-dev
-gem sources -a http://gems.github.com
-gem install -v=2.3.4 rails mysql macaddr RedCloth simple-rss twitter4r --no-rdoc
+sudo apt-get install ruby rubygems irb ri rdoc ruby1.8-dev build-essential libmysql-ruby libopenssl-ruby
+sudo gem source -a http://gemcutter.org/ 
+sudo gem source -a http://gems.github.com
+sudo gem install -v=2.3.4 rails macaddr RedCloth simple-rss twitter4r --no-rdoc
 rake db:create RAILS_ENV=production
 rake db:migrate RAILS_ENV=production
+
+# Install Nginx + Passenger
+sudo gem install passenger
+
