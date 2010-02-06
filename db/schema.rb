28db088cdd4a4c54e44a86ecfdac385fe17c7e2f
Jason Lee <huacnlee@gmail.com>
2010-02-06 11:59:55 +0800

    add default generate migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100206000001) do
+ActiveRecord::Schema.define(:version => 20100206000002) do
 
   create_table "categories", :force => true do |t|
     t.string   "name",        :null => false
