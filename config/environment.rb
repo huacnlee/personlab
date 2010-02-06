2299eb2f47a78e72b075481e10f6ec6c1f4f4f16
Jason Lee <huacnlee@gmail.com>
2010-02-06 11:01:48 +0800

    remove will_paginate plugin files

--- config/environment.rb
+++ config/environment.rb
@@ -14,11 +14,6 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
-  # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -46,7 +41,7 @@ Rails::Initializer.run do |config|
 	config.gem 'macaddr', :version => '~> 1.0'
 	config.gem 'RedCloth'
 	config.gem 'simple-rss'
-	# config.gem 'twitter4r'
+	config.gem 'twitter4r'
 
 end
 
