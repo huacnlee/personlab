e13991aa31301692f31f1b0bdb870733dca387d6
Jason Lee <huacnlee@gmail.com>
2010-05-25 07:17:04 +0800

    fixed backup script with mutt send mail

--- autobackup.sh
+++ autobackup.sh
@@ -4,22 +4,14 @@ BACKUP_PATH=/tmp/autobackups
 DATE_NAME=`date +%y%m%d`
 mkdir $BACKUP_PATH
 # package
-# mysqldump -umonster -pytrip123123 --databases pasite > $BACKUP_PATH/pasite.sql
+mysqldump -umonster -pytrip123123 --databases pasite > $BACKUP_PATH/pasite.sql
 mysqldump -umonster -pytrip123123 --databases personlab_production > $BACKUP_PATH/personlab.sql
-# tar zcf $BACKUP_PATH/pasite_db_$DATE_NAME.tar.gz $BACKUP_PATH/pasite.sql
+tar zcf $BACKUP_PATH/pasite_db_$DATE_NAME.tar.gz $BACKUP_PATH/pasite.sql
 tar zcf $BACKUP_PATH/personlab_db_$DATE_NAME.tar.gz $BACKUP_PATH/personlab.sql
-# tar zcf $BACKUP_PATH/pasite_src_$DATE_NAME.tar.gz /home/jason/wwwroot/pasite/
-tar zcf $BACKUP_PATH/personlab_src_$DATE_NAME.tar.gz /home/jason/wwwroot/personlab/
 
 # sendmail
-# echo "pasite.org db backup" | mutt -a $BACKUP_PATH/pasite_db_$DATE_NAME.tar.gz -s "pasite.org db backup" huacnlee@gmail.com
-# echo "pasite.org source backup" | mutt -a $BACKUP_PATH/pasite_src_$DATE_NAME.tar.gz -s "pasite.org source backup" huacnlee@gmail.com
-echo "huacnlee.com db backup" | mutt -a $BACKUP_PATH/personlab_db_$DATE_NAME.tar.gz -s "huacnlee.com db backup" huacnlee@gmail.com
-echo "huacnlee.com source backup" | mutt -a $BACKUP_PATH/personlab_src_$DATE_NAME.tar.gz -s "huacnlee.com source backup" huacnlee@gmail.com
-
-# remove website runtime log
-# sudo rm /home/jason/wwwroot/pasite/log/*
-sudo rm /home/jason/wwwroot/personlab/log/*
+echo "pasite.org db backup" | mutt -a $BACKUP_PATH/pasite_db_$DATE_NAME.tar.gz -s "pasite.org db backup" -c huacnlee@gmail.com
+echo "huacnlee.com db backup" | mutt -a $BACKUP_PATH/personlab_db_$DATE_NAME.tar.gz -s "huacnlee.com db backup" -c huacnlee@gmail.com
 
 # clear tmp file
 rm -R $BACKUP_PATH
