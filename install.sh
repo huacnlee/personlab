c4becd3e5d2170de08e91feda56267589cd06c12
Jason Lee <huacnlee@gmail.com>
2010-05-26 22:48:41 +0800

    update install script

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 sudo apt-get install ruby rubygems irb ri rdoc ruby1.8-dev build-essential libmysql-ruby libopenssl-ruby
 sudo gem source -a http://gemcutter.org/ 
 sudo gem source -a http://gems.github.com
-sudo gem install -v=2.3.4 rails macaddr RedCloth simple-rss twitter4r --no-rdoc
+sudo gem install -v=2.3.5 rails macaddr RedCloth simple-rss twitter4r --no-rdoc
 rake db:create RAILS_ENV=production
 rake db:migrate RAILS_ENV=production
-rake db:create_admin
+rake db:seed RAILS_ENV=production
+rake captcha:generate COUNT=250
 
 # Install Nginx + Passenger
 sudo gem install passenger
