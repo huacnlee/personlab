a5eb6295946b524a1d1b0962c54accee1888cbd9
Jason Lee <huacnlee@gmail.com>
2010-07-22 17:38:10 +0800

    update autobackup.sh

--- autobackup.sh
+++ autobackup.sh
@@ -6,15 +6,12 @@ mkdir $BACKUP_PATH
 # package
 mysqldump -umonster -pytrip123123 --databases pasite > $BACKUP_PATH/pasite.sql
 mysqldump -umonster -pytrip123123 --databases personlab_production > $BACKUP_PATH/personlab.sql
-mysqldump -umonster -pytrip123123 --databases tinytopic > $BACKUP_PATH/tinytopic.sql
 tar zcf $BACKUP_PATH/pasite_db_$DATE_NAME.tar.gz $BACKUP_PATH/pasite.sql
 tar zcf $BACKUP_PATH/personlab_db_$DATE_NAME.tar.gz $BACKUP_PATH/personlab.sql
-tar zcf $BACKUP_PATH/tinytopic_db_$DATE_NAME.tar.gz $BACKUP_PATH/tinytopic.sql
 
 # sendmail
 echo "pasite.org db backup" | mutt -a $BACKUP_PATH/pasite_db_$DATE_NAME.tar.gz -s "pasite.org db backup" -c huacnlee@gmail.com
 echo "huacnlee.com db backup" | mutt -a $BACKUP_PATH/personlab_db_$DATE_NAME.tar.gz -s "huacnlee.com db backup" -c huacnlee@gmail.com
-echo "tinytopic.com db backup" | mutt -a $BACKUP_PATH/tinytopic_db_$DATE_NAME.tar.gz -s "tinytopic.com db backup" -c huacnlee@gmail.com
 
 # clear tmp file
 rm -R $BACKUP_PATH
