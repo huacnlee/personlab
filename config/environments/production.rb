ebaa9abf3adda378a1d3634df6e54f4203877097
Jason Lee <huacnlee@gmail.com>
2010-11-30 15:24:58 +0800

    Disable x-sendfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Personlab::Application.configure do
   
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile"
   
   # If you have no front-end server that supports something like X-Sendfile,
   # just comment this out and Rails will serve the files
