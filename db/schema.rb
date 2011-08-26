c10c17c5f55f04f96259f397b4c27dd8b6c65565
Jason Lee <huacnlee@gmail.com>
2011-08-26 11:59:18 +0800

    Add indexes for tables;

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101214015858) do
+ActiveRecord::Schema.define(:version => 20110826035412) do
 
   create_table "categories", :force => true do |t|
     t.string   "name",        :null => false
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(:version => 20101214015858) do
     t.datetime "updated_at"
     t.integer  "posts_count"
   end
+
+  add_index "categories", ["slug"], :name => "index_categories_on_slug"
 
   create_table "comments", :force => true do |t|
     t.integer  "post_id",    :null => false
@@ -33,6 +35,8 @@ ActiveRecord::Schema.define(:version => 20101214015858) do
     t.string   "user_agent"
     t.string   "referrer"
   end
+
+  add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
 
   create_table "menus", :force => true do |t|
     t.string   "name",                          :null => false
@@ -51,6 +55,8 @@ ActiveRecord::Schema.define(:version => 20101214015858) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "pages", ["slug"], :name => "index_pages_on_slug"
 
   create_table "post_fellow", :force => true do |t|
     t.string   "email",      :null => false
@@ -72,6 +78,10 @@ ActiveRecord::Schema.define(:version => 20101214015858) do
     t.string   "meta_description"
     t.integer  "category_id"
   end
+
+  add_index "posts", ["category_id"], :name => "index_posts_on_category_id"
+  add_index "posts", ["slug"], :name => "index_posts_on_slug"
+  add_index "posts", ["status"], :name => "index_posts_on_status"
 
   create_table "settings", :force => true do |t|
     t.string   "site_name"
@@ -141,6 +151,8 @@ ActiveRecord::Schema.define(:version => 20101214015858) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "unfollowers", ["unfollowerable_id", "unfollowerable_type"], :name => "index_unfollowers_on_unfollowerable_id_and_unfollowerable_type"
 
   create_table "users", :force => true do |t|
     t.string   "uname",      :null => false
