10e43a85ce9c567f9bfa52a6146d4d799363a902
Jason Lee <huacnlee@gmail.com>
2011-10-25 13:21:26 +0800

    Version to 0.8

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-APP_VERSION = "0.7.8"
+APP_VERSION = "0.8"
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
