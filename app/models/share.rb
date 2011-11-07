4b0471c2f4642078553bab90f0997f0bde9a745d
Jason Lee <huacnlee@gmail.com>
2011-11-07 14:47:07 +0800

    Add twitter page. Share page cache fix.

--- app/models/share.rb
+++ app/models/share.rb
@@ -3,30 +3,25 @@ require "rubygems"
 require 'simple-rss'
 require 'open-uri'
 
-class Share < ActiveRecord::Base
+class Share
   
   def self.api_url(setting)     
     "http://www.google.com/reader/public/atom/user%2F#{setting.google_reader_id}%2Fstate%2Fcom.google%2Fbroadcast"
   end
   
   def self.find_all(setting,force = false)
-    cache_key = "data/share_items"
-    feeds = Rails.cache.read(cache_key)
-    if not feeds or force
-      feeds = []
-      feed = SimpleRSS::parse open(api_url(setting))
-      if feed != nil
-        feed.items.each do |item|
-					continue if item.blank?
-          feeds << {  
-            "title" => item.title,
-            "link" => item.link,
-            "updated_at" => item.updated,
-            "author" => item.author,
-          }
-        end
+    feeds = []
+    feed = SimpleRSS::parse open(api_url(setting))
+    if feed != nil
+      feed.items.each do |item|
+				continue if item.blank?
+        feeds << {  
+          "title" => item.title,
+          "link" => item.link,
+          "updated_at" => item.updated,
+          "author" => item.author,
+        }
       end
-      Rails.cache.write(cache_key,feeds, :expires_in => 1.days)
     end
     feeds
   end
